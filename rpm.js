5b93d86a6f7ced2ccccdd32b9c2f9a174575a19a
Max-Lovell <max_lovell@hotmail.co.uk>
2025-01-29 12:00:21 +0000

    Call image preloading function

--- rpm.js
+++ rpm.js
@@ -11,10 +11,19 @@ function shuffle(array) {
     }
 }
 
+function preloadImages(){
+    for(let p=0; p<stimIds.length; p++){
+        const imageStub = 'https://raw.githubusercontent.com/nivlab/jspsych-demos/main/tasks/rpm/img/'  + stimIds[p]+'.png'
+        new Image().src = imageStub;
+        for(let t=0; t<=8; t++){
+            new Image().src = imageStub+ '_' + (t+1) + '.png'
+        }
+    }
+}
+
 function attachListeners(){
     function clickListener(e){
         data.push({pattern: stimIds[trialN], selectedStim: e.target.id})
-        console.log(data)
         runNextTrial()
     }
     const targets = document.getElementById('targetsGrid').children
@@ -29,10 +38,9 @@ async function runNextTrial(){
         return
     }
     const stimId = stimIds[trialN]
-    const imageStub = 'img/' + stimId 
+    const imageStub = 'https://raw.githubusercontent.com/nivlab/jspsych-demos/main/tasks/rpm/img/'  + stimId //'img/' + stimId 
     document.getElementById('pattern').src = imageStub + '.png'
     const targets = document.getElementById('targetsGrid').children
-    console.log(targets)
     for(let i=0; i<targets.length; i++){
         targets[i].src = imageStub + '_' + (i+1) + '.png'
     }
@@ -44,7 +52,7 @@ function displayData(){
     document.getElementById('dataDisplay').innerText = JSON.stringify(data, null, "\t")
 }
 
+preloadImages()
 shuffle(stimIds)
 attachListeners()
 runNextTrial()
-
